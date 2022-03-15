ce699e663cd487ab1f834cd5afc7bfc8ff646add
yuchendong <68779035+yuchendong13@users.noreply.github.com>
2022-03-15 18:54:19 +1300

    Fix conventions
    - Moved general css (body, h1) to file-specific class names
    - Added hp identifiers to homepage class names

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -6,38 +6,41 @@ import Grid from '@mui/material/Grid';
 
 const Homepage = () => {
   return (
-    <>
-      <h1>NavBar</h1> {/* Placeholder for Navbar component - to be replaced */}
+    <div className="hpHomePage">
+      <h1 className="hpTitle">NavBar</h1> {/* Placeholder for Navbar component - to be replaced */}
       <Container maxWidth="md">
-        <h1>Popular Communities</h1>
-        <div className="divide">
+        <h1 className="hpTitle">Popular Communities</h1>
+        <div className="hpDivide">
           <hr className="hpDivider"></hr>
         </div>
-        <Container maxWidth="md">
-          {/* Placeholder for Scrollable Popular Communities components - grid to be replaced*/}
-          <Grid container spacing={5} justify="center">
-            <Grid item xs={12} sm={6} md={4}>
-              <PopularCommunity name="SOFTENG 701" alt="SOFTENG 701" image={require("./SOFTENG701.png")}/>
+        <div className="hpContainer">
+          <Container maxWidth="md">
+            {/* Placeholder for Scrollable Popular Communities components - grid to be replaced*/}
+            <Grid container spacing={5} justify="center">
+              <Grid item xs={12} sm={6} md={4}>
+                <PopularCommunity name="SOFTENG 701" alt="SOFTENG 701" image={require("./SOFTENG701.png")}/>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+              <PopularCommunity name="SOFTENG 754" alt="SOFTENG 754" image={require("./SOFTENG754.png")}/>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+              <PopularCommunity name="COMPSYS 726" alt="COMPSYS 726" image={require("./COMPSYS726.png")}/>
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-            <PopularCommunity name="SOFTENG 754" alt="SOFTENG 754" image={require("./SOFTENG754.png")}/>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-            <PopularCommunity name="COMPSYS 726" alt="COMPSYS 726" image={require("./COMPSYS726.png")}/>
-            </Grid>
-          </Grid>
-        </Container> 
-        
+          </Container> 
+        </div>
         <br></br>
-        <h1>Popular Posts</h1>
-        <div className="divide">
+        <h1 className="hpTitle">Popular Posts</h1>
+        <div className="hpDivide">
           <hr className="hpDivider"></hr>
         </div>
-        <Container maxWidth="md">
-          Popular posts feed in this container {/* Placeholder for Popular Posts card components - to be replaced */}
-        </Container>
+        <div className="hpContainer">
+          <Container maxWidth="md">
+            Popular posts feed in this container {/* Placeholder for Popular Posts card components - to be replaced */}
+          </Container>
+        </div>
       </Container>
-    </>
+    </div>
   );
 };
 

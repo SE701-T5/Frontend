ab6dcd03d5c959f30479331fa645f7318acbc20a
yuchendong <68779035+yuchendong13@users.noreply.github.com>
2022-03-16 10:35:27 +1300

    Issue #12 homepage layout (#21)
    * Initial homepage layout
    
    - Includes Popular Community reusable component cards
    - Setup containers for homepage elements
    - Placeholder text for Popular Posts cards
    - Placeholder text for navbar
    
    * Added dividers to homepage titles
    
    * Removed unused import and using deconstructed values
    
    * Changed to fit Figma better (read desc)
    
    - Changed container width to be narrower as per Figma design
    - Changed body text colour
    - Changed divider thickness and colour
    - Changed css targeting to className
    
    * Tidy code
    
    * Added comments and cleaned "Popular Communities" card
    
    - Removed 'raised' prop to closer resemble the Figma design
    - Added comments for placeholders and reusable component description
    
    * Fix conventions
    
    - Moved general css (body, h1) to file-specific class names
    - Added hp identifiers to homepage class names
    
    * Removed nav bar placeholder
    
    Co-authored-by: christopher-alba <calb200@aucklanduni.ac.nz>

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,8 +1,46 @@
 import React from "react";
 import "./homepage.css";
+import Container from '@mui/material/Container';
+import PopularCommunity from "../../components/PopularCommunity/PopularCommunity.jsx";
+import Grid from '@mui/material/Grid';
 
 const Homepage = () => {
-  return <h1>Homepage</h1>;
+  return (
+    <div className="hpHomePage">
+      <Container maxWidth="md">
+        <h1 className="hpTitle">Popular Communities</h1>
+        <div className="hpDivide">
+          <hr className="hpDivider"></hr>
+        </div>
+        <div className="hpContainer">
+          <Container maxWidth="md">
+            {/* Placeholder for Scrollable Popular Communities components - grid to be replaced*/}
+            <Grid container spacing={5} justify="center">
+              <Grid item xs={12} sm={6} md={4}>
+                <PopularCommunity name="SOFTENG 701" alt="SOFTENG 701" image={require("./SOFTENG701.png")}/>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+              <PopularCommunity name="SOFTENG 754" alt="SOFTENG 754" image={require("./SOFTENG754.png")}/>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+              <PopularCommunity name="COMPSYS 726" alt="COMPSYS 726" image={require("./COMPSYS726.png")}/>
+              </Grid>
+            </Grid>
+          </Container> 
+        </div>
+        <br></br>
+        <h1 className="hpTitle">Popular Posts</h1>
+        <div className="hpDivide">
+          <hr className="hpDivider"></hr>
+        </div>
+        <div className="hpContainer">
+          <Container maxWidth="md">
+            Popular posts feed in this container {/* Placeholder for Popular Posts card components - to be replaced */}
+          </Container>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Homepage;

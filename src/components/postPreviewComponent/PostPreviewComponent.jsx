01c253f4033ca7a70815d22f253747f6bf11ea5d
Tvo465 <tvo465@aucklanduni.ac.nz>
2022-03-15 17:40:56 +1300

    made the image slider a reusable component

--- src/components/postPreviewComponent/PostPreviewComponent.jsx
+++ src/components/postPreviewComponent/PostPreviewComponent.jsx
@@ -1,83 +1,80 @@
 import React from "react";
 import "./postPreviewComponent.css";
-import { ArrowUpward, ArrowDownward } from '@mui/icons-material';
+import { ArrowUpward, ArrowDownward, AddComment } from '@mui/icons-material';
 import { Box, Button, IconButton } from '@mui/material';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import { Carousel } from 'react-responsive-carousel';
 
 
 const PostPreviewComponent = ({ post }) => {
-    const { title, community, upi, time, text, upvotes, downvotes } = post;
-    
+    const { title, community, upi, time, text, upvotes, downvotes, images } = post;
+
     return (
-        <Box className="postArea">
-            
-                <Box>
-                    <Box className="row1">
+        <Box className="ppc-postArea">
+
+            <Box>
+                <Box className="ppc-row1">
+
+                    <Box>
+                        <h5 className="ppc-course">{community}</h5>
+                    </Box>
+
+                    <Box className="ppc-right">
+                        <Box>
+                            <p className="ppc-upi">{upi}</p>
+                        </Box>
 
                         <Box>
-                            <h5 class="course">{community}</h5>
-                        </Box>
-
-                        <Box className="right">
-                            <Box>
-                                <p className="upi">{upi}</p>
-                            </Box>
-
-                            <Box>
-                                <p className="timeposted">{time}</p>
-                            </Box>
-                        </Box>
-
-                    </Box>
-
-
-                    <Box className="row2">
-                        <h2 className="title">{title}</h2>
-                        <Box >
-                            <p class="text">{text}</p>
+                            <p className="ppc-timeposted">{time}</p>
                         </Box>
                     </Box>
+
                 </Box>
 
-                <Carousel dynamicHeight showThumbs={false}>
-                <Box>
-                    <img alt="uoaimage" src="./uoa.jpg" />
-                    
+
+                <Box className="ppc-row2">
+                    <h2 className="ppc-title">{title}</h2>
+                    <Box >
+                        <p className="ppc-text">{text}</p>
+                    </Box>
                 </Box>
-                <Box>
-                    <img alt="uoaimage" src="./uoa.jpg" />
-                    
-                </Box>
-                <Box>
-                    <img alt="uoaimage" src="./uoa.jpg" />
-                    
-                </Box>
+            </Box>
+
+            <Carousel dynamicHeight showThumbs={false} autoPlay infiniteLoop>
+                {
+                    images.map(image => {
+                        return (
+                            <Box>
+                                <img alt="uoaimage" src={image} />
+                            </Box>
+                        )
+                    })
+                }
             </Carousel>
 
 
-                <Box className="row3">
+            <Box className="ppc-row3">
 
-                    <Box className="votes">
-                        <IconButton>
-                            <ArrowUpward sx={{ color: "rgba(0,128,167,0.35)", "&:hover": { color: "#0080A7" } }} />
-                        </IconButton>
+                <Box className="ppc-votes">
+                    <IconButton>
+                        <ArrowUpward sx={{ color: "rgba(0,128,167,0.35)", "&:hover": { color: "#0080A7" } }} />
+                    </IconButton>
 
-                        <p className="numofvotes">{upvotes - downvotes}</p>
+                    <p className="ppc-numofvotes">{upvotes - downvotes}</p>
 
-                        <IconButton>
-                            <ArrowDownward sx={{ color: "rgba(0,128,167,0.35)", "&:hover": { color: "#0080A7" } }} />
-                        </IconButton>
+                    <IconButton>
+                        <ArrowDownward sx={{ color: "rgba(0,128,167,0.35)", "&:hover": { color: "#0080A7" } }} />
+                    </IconButton>
 
-                    </Box>
-
-                    <Box>
-                        <Button variant="text">Comments</Button>
-                    </Box>
                 </Box>
 
+                <Box>
+                    <Button variant="outlined" startIcon={<AddComment/>}>Comment</Button>
+                </Box>
+            </Box>
 
-            
+
+
         </Box>);
 };
 

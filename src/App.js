6d516f2ce0ae711ff6dc9d1b81303444994293ad
yuchendong <68779035+yuchendong13@users.noreply.github.com>
2022-03-16 19:18:37 +1300

    New post page layout
    - contains text box for title
    - contains dropdown to select forum the post is for
    - contains text area for post description
    - contains upload media button for pictures

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import ComponentsTesting from "./pages/componentsTesting/ComponentsTesting";
 import Homepage from "./pages/Homepage";
+import NewPost from "./pages/newPost/NewPost.jsx";
 import NavBar from "./components/NavBar/NavBar";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <Routes>
       <Route exact path="/" element={<Homepage />} />
       <Route exact path="/ComponentsTesting" element={<ComponentsTesting />} />
+      <Route exact path="/NewPost" element={<NewPost />} />
     </Routes>
    </>
   );

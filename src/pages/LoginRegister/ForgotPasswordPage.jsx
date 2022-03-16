b5d68bcf713337b7121335e3d36c73e84bc77292
Xiaoqi510 <100509492+Xiaoqi510@users.noreply.github.com>
2022-03-16 18:33:38 +1300

    Update and rename ForgotPasswordPage.js to ForgotPasswordPage.jsx

--- src/pages/LoginRegister/ForgotPasswordPage.jsx
+++ src/pages/LoginRegister/ForgotPasswordPage.jsx
@@ -5,7 +5,7 @@ import "../../App.css";
 
 export default function ForgotPasswordPage() {
   return (
-    <div className="text-center m-5-auto">
+    <div className="text-center m-5-auto ">
       <h2>Reset your password</h2>
       <h5>
         Enter your email address and we will send you a link to reset your
@@ -15,20 +15,26 @@ export default function ForgotPasswordPage() {
         <p>
           <label id="reset_pass_lbl">Email address</label>
           <br />
-          <input type="email" name="email" required />
+          <input
+            className="LoginRegisterForm_input"
+            type="email"
+            name="email"
+            required
+          />
         </p>
         <p>
-          <button id="sub_btn" type="submit">
+          <button
+            className="LoginRegisterForm_button"
+            id="sub_btn"
+            type="submit"
+          >
             Send password reset email
           </button>
         </p>
       </form>
       <footer>
-        <p>
+        <p className="LoginRegisterForm_p">
           First time? <Link to="/register">Create an account</Link>.
-        </p>
-        <p>
-          <Link to="/">Back to Homepage</Link>.
         </p>
       </footer>
     </div>

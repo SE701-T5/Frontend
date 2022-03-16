2bb96527c39967c8473ace7f700dde560700f9e2
Xiaoqi510 <100509492+Xiaoqi510@users.noreply.github.com>
2022-03-16 18:38:48 +1300

    Update and rename RegisterPage.js to RegisterPage.jsx

--- src/pages/LoginRegister/RegisterPage.jsx
+++ src/pages/LoginRegister/RegisterPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import "./LoginPage.css";
 import AuthContext from "../../context/AuthProvider";
 import RegisterForm from "./RegisterForm";
@@ -6,26 +6,24 @@ import { useNavigate } from "react-router-dom";
 
 export default function RegisterPage() {
   const navigate = useNavigate();
-  const [errors] = useState("");
-  const { userDetails } = useContext(AuthContext);
-  /*testing array
-  const Users = [
-    {
-      email: "admin@123",
-      username: "admin",
-      password: "12ABqwer",
-    },
-    { email: "aa@123", username: "scsf", password: "12ABqwer" },
-  ];*/
 
-  const [isSuccess] = useState(false);
+  const { userDetails, setUserDetails } = useContext(AuthContext);
 
   const Register = (details) => {
-    console.log(userDetails);
     console.log(details);
+    console.log(userDetails);
     if (!userDetails.find((user) => user.email === details.email)) {
       if (details.password1 === details.password2) {
         console.log("Registered successfully!");
+        console.log(userDetails);
+        setUserDetails((preuserDetails) => [
+          ...preuserDetails,
+          {
+            email: details.email,
+            username: details.username,
+            password: details.password1,
+          },
+        ]);
         console.log(userDetails);
         alert("Registered successfully! Please Log in!");
         navigate("/login");
@@ -39,23 +37,5 @@ export default function RegisterPage() {
     }
   };
 
-  return (
-    <>
-      {isSuccess ? (
-        <div>
-          <h1>Registered Successfully!</h1>
-          <br />
-          <a href="/login">
-            <button id="login">Log in</button>
-          </a>
-        </div>
-      ) : (
-        <RegisterForm
-          Register={Register}
-          errors={errors}
-          userDetails={userDetails}
-        />
-      )}
-    </>
-  );
+  return <>{<RegisterForm Register={Register} />}</>;
 }

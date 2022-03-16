6cffa9f326c70156b7c91f6ae8c76dcf3cf019fd
yuchendong <68779035+yuchendong13@users.noreply.github.com>
2022-03-16 21:41:06 +1300

    New post page layout (#30)
    * New post page layout
    
    - contains text box for title
    - contains dropdown to select forum the post is for
    - contains text area for post description
    - contains upload media button for pictures
    
    * Added title, removed background div
    
    * Rounded corners and fixed overlapping border and title in selection
    
    Co-authored-by: christopher-alba <calb200@aucklanduni.ac.nz>

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import ComponentsTesting from "./pages/componentsTesting/ComponentsTesting";
 import Homepage from "./pages/Homepage";
-
+import NewPost from "./pages/newPost/NewPost.jsx";
 import ForgotPasswordPage from "./pages/LoginRegister/ForgotPasswordPage";
 import LoginPage from "./pages/LoginRegister/LoginPage";
 import RegisterPage from "./pages/LoginRegister/RegisterPage";
-
 import NavBar from "./components/NavBar/NavBar";
 
 
@@ -17,14 +16,12 @@ function App() {
     <NavBar />
     <Routes>
       <Route exact path="/" element={<Homepage />} />
-
       <Route exact path="/homepage" element={<Homepage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-
       <Route exact path="/ComponentsTesting" element={<ComponentsTesting />} />
-
+      <Route exact path="/NewPost" element={<NewPost />} />
     </Routes>
    </>
   );

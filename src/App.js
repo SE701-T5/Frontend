3f012ab6f572d4ef7f5c6a57b582d1838c17c745
Chai-xiaoqi <76461133+Chai-xiaoqi@users.noreply.github.com>
2022-03-16 21:14:19 +1300

    Login (#25)
    * add login and register function
    
    * Update AuthProvider.js
    
    * Update LoginForm.js
    
    delete unnecessary variable
    
    * Update unnecessary variables
    
    * Delete unnecessary variables
    
    * Update unnecessary variables
    
    * Update AuthProvider
    
    add LocalStorage and useContext
    
    * Update and rename ForgotPasswordPage.js to ForgotPasswordPage.jsx
    
    * Update and rename LoginForm.js to LoginForm.jsx
    
    * Update Login.css
    
    * Update LoginPage
    
    * Update RegisterForm
    
    * Update and rename RegisterPage.js to RegisterPage.jsx
    
    * Update LoginPage.css
    
    * Update RegisterForm.jsx
    
    * Update LoginForm.jsx
    
    * remove  background: #e5e5e5;
    
    * add background: #e5e5e5;
    
    Co-authored-by: Xiaoqi510 <100509492+Xiaoqi510@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import ComponentsTesting from "./pages/componentsTesting/ComponentsTesting";
 import Homepage from "./pages/Homepage";
+
+import ForgotPasswordPage from "./pages/LoginRegister/ForgotPasswordPage";
+import LoginPage from "./pages/LoginRegister/LoginPage";
+import RegisterPage from "./pages/LoginRegister/RegisterPage";
+
 import NavBar from "./components/NavBar/NavBar";
+
 
 function App() {
   return (
@@ -11,7 +17,14 @@ function App() {
     <NavBar />
     <Routes>
       <Route exact path="/" element={<Homepage />} />
+
+      <Route exact path="/homepage" element={<Homepage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+
       <Route exact path="/ComponentsTesting" element={<ComponentsTesting />} />
+
     </Routes>
    </>
   );
---
+++ src/pages/LoginRegister/LoginPage.jsx
@@ -0,0 +1,41 @@
+import React, { useContext } from "react";
+import "./LoginPage.css";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../../context/AuthProvider";
+import LoginForm from "./LoginForm";
+import Homepage from "../Homepage";
+export default function LoginPage() {
+  const navigate = useNavigate();
+
+  const { userDetails, login, authorized } = useContext(AuthContext);
+
+  const LoginFormDetail = (details) => {
+    const founduser = userDetails.find((user) => user.email === details.email);
+    if (founduser) {
+      if (details.password === founduser.password) {
+        console.log("loged in");
+        alert("Logged in!");
+        login();
+        navigate("/homepage");
+      } else {
+        console.log("Your password is wrong!");
+        alert("Your password is wrong!");
+      }
+    } else {
+      console.log("No such account, Please register first!");
+      alert("No such account, Please register first!");
+    }
+  };
+  return (
+    <>
+      {authorized ? (
+        <div>
+          {" "}
+          <Homepage />
+        </div>
+      ) : (
+        <LoginForm LoginFormDetail={LoginFormDetail} />
+      )}
+    </>
+  );
+}

8cfb3b62faf58d5f8e811e8558ef4d05e9dcbe10
Tvo465 <tvo465@aucklanduni.ac.nz>
2022-03-18 19:18:06 +1300

    A search filter function is added for title filtering, still need to add in the filtering for texts but this will be made into a new issue

--- src/pages/Posts/Posts.jsx
+++ src/pages/Posts/Posts.jsx
@@ -1,53 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./posts.css";
 import Container from "@mui/material/Container";
 import { Search } from "@mui/icons-material";
-import { Box, IconButton, Avatar, Button, FormControl,InputLabel, OutlinedInput } from "@mui/material";
+import { Box, IconButton, Avatar, Button, FormControl, InputLabel, OutlinedInput, InputAdornment } from "@mui/material";
 import PostPreviewComponent from "../../components/postPreviewComponent";
-
+const posts = [{
+    title: "yes",
+    community: "Softeng701",
+    upi: "shfdj364",
+    time: "12:12",
+    text: "I love react and react libraries but I have never used Semantic UI...  I heard from a teammate  that it’s pretty easy to use and has a lot of documentation so hopefully I’ll be fine. Any tips...",
+    upvotes: 24,
+    downvotes: 23,
+    images: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXsGYUdnO7UWSuZV_wMfyq-ChTkvfHjMMUcA&usqp=CAU", "https://nz3.architecturemedia.net/site_media/media/cache/52/47/5247e80bab99158eecfb84da220fe7b1.jpg"]
+},
+{
+    title: "no",
+    community: "Softeng701",
+    upi: "shfdj364",
+    time: "12:12",
+    text: "I love react and react libraries but I have never used Semantic UI...  I heard from a teammate  that it’s pretty easy to use and has a lot of documentation so hopefully I’ll be fine. Any tips...",
+    upvotes: 24,
+    downvotes: 23,
+    images: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXsGYUdnO7UWSuZV_wMfyq-ChTkvfHjMMUcA&usqp=CAU", "https://nz3.architecturemedia.net/site_media/media/cache/52/47/5247e80bab99158eecfb84da220fe7b1.jpg"]
+},
+{
+    title: "hi",
+    community: "Softeng701",
+    upi: "shfdj364",
+    time: "12:12",
+    text: "I love react and react libraries but I have never used Semantic UI...  I heard from a teammate  that it’s pretty easy to use and has a lot of documentation so hopefully I’ll be fine. Any tips...",
+    upvotes: 24,
+    downvotes: 23,
+    images: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXsGYUdnO7UWSuZV_wMfyq-ChTkvfHjMMUcA&usqp=CAU", "https://nz3.architecturemedia.net/site_media/media/cache/52/47/5247e80bab99158eecfb84da220fe7b1.jpg"]
+},
+{
+    title: "title4",
+    community: "Softeng701",
+    upi: "shfdj364",
+    time: "12:12",
+    text: "I love react and react libraries but I have never used Semantic UI...  I heard from a teammate  that it’s pretty easy to use and has a lot of documentation so hopefully I’ll be fine. Any tips...",
+    upvotes: 24,
+    downvotes: 23,
+    images: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXsGYUdnO7UWSuZV_wMfyq-ChTkvfHjMMUcA&usqp=CAU", "https://nz3.architecturemedia.net/site_media/media/cache/52/47/5247e80bab99158eecfb84da220fe7b1.jpg"]
+}
+]
 const Posts = () => {
 
-    const posts = [{
-        title: "sjngosnhpmre",
-        community: "Softeng701",
-        upi: "shfdj364",
-        time: "12:12",
-        text: "I love react and react libraries but I have never used Semantic UI...  I heard from a teammate  that it’s pretty easy to use and has a lot of documentation so hopefully I’ll be fine. Any tips...",
-        upvotes: 24,
-        downvotes: 23,
-        images: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXsGYUdnO7UWSuZV_wMfyq-ChTkvfHjMMUcA&usqp=CAU", "https://nz3.architecturemedia.net/site_media/media/cache/52/47/5247e80bab99158eecfb84da220fe7b1.jpg"]
-    },
-    {
-        title: "sjngosnhpmre",
-        community: "Softeng701",
-        upi: "shfdj364",
-        time: "12:12",
-        text: "I love react and react libraries but I have never used Semantic UI...  I heard from a teammate  that it’s pretty easy to use and has a lot of documentation so hopefully I’ll be fine. Any tips...",
-        upvotes: 24,
-        downvotes: 23,
-        images: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXsGYUdnO7UWSuZV_wMfyq-ChTkvfHjMMUcA&usqp=CAU", "https://nz3.architecturemedia.net/site_media/media/cache/52/47/5247e80bab99158eecfb84da220fe7b1.jpg"]
-    },
-    {
-        title: "sjngosnhpmre",
-        community: "Softeng701",
-        upi: "shfdj364",
-        time: "12:12",
-        text: "I love react and react libraries but I have never used Semantic UI...  I heard from a teammate  that it’s pretty easy to use and has a lot of documentation so hopefully I’ll be fine. Any tips...",
-        upvotes: 24,
-        downvotes: 23,
-        images: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXsGYUdnO7UWSuZV_wMfyq-ChTkvfHjMMUcA&usqp=CAU", "https://nz3.architecturemedia.net/site_media/media/cache/52/47/5247e80bab99158eecfb84da220fe7b1.jpg"]
-    },
-    {
-        title: "sjngosnhpmre",
-        community: "Softeng701",
-        upi: "shfdj364",
-        time: "12:12",
-        text: "I love react and react libraries but I have never used Semantic UI...  I heard from a teammate  that it’s pretty easy to use and has a lot of documentation so hopefully I’ll be fine. Any tips...",
-        upvotes: 24,
-        downvotes: 23,
-        images: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXsGYUdnO7UWSuZV_wMfyq-ChTkvfHjMMUcA&usqp=CAU", "https://nz3.architecturemedia.net/site_media/media/cache/52/47/5247e80bab99158eecfb84da220fe7b1.jpg"]
-    }
-    ]
+    //creating a state for filtered results
+    const [filteredResults, setFilteredResults] = useState(posts);
+
+    //Function for filtering posts by title when user inputs text in the search bar, else all posts will be shown
+    const searchItem = (searchValue) => {
+        if (searchValue !== '') {
+            const filteredSearch = posts.filter((item) => { return item.title.toLowerCase().includes(searchValue.toLowerCase()) })
+            setFilteredResults(filteredSearch)
+        }
+        else {
+            setFilteredResults(posts)
+        }
+    } 
+  
     return (
         <Container maxWidth="md">
             <Box className="p-toprow">
@@ -64,19 +77,21 @@ const Posts = () => {
                     <OutlinedInput
                         id="search"
                         endAdornment={
-                            <inputAdornment position="end">
+                            <InputAdornment position="end">
                                 <Search />
-                            </inputAdornment>
+                            </InputAdornment>
 
                         }
+                        //when user inputs something into the search bar this calls the searchItem function to filter out the posts
+                        //based on the input
+                        onChange={(e) => searchItem(e.target.value)}
                     />
                 </FormControl>
             </Box>
-     
-            {posts.map(post =>
+                        
+            {filteredResults && filteredResults.map(post =>
                 <Box className="p-post">
                     <PostPreviewComponent post={post} />
-
                 </Box>)}
         </Container>
     )

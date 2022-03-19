bc5b4368312f0fb29f964eed791ec3a7044d4f6a
christopher-alba <calb200@aucklanduni.ac.nz>
2022-03-19 21:33:56 +1300

    Pull/issue47 (#49)
    * add link to new post
    
    * add ability to search for community, text, and title of posts
    
    * allow filtering by upi aswell
    
    * add basedon search query text
    
    * add results counter in message

--- src/pages/Posts/Posts.jsx
+++ src/pages/Posts/Posts.jsx
@@ -2,99 +2,136 @@ import React, { useState } from "react";
 import "./posts.css";
 import Container from "@mui/material/Container";
 import { Search } from "@mui/icons-material";
-import { Box, Button, FormControl, InputLabel, OutlinedInput, InputAdornment } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+} from "@mui/material";
 import PostPreviewComponent from "../../components/postPreviewComponent";
-const posts = [{
+import { useNavigate } from "react-router-dom";
+const posts = [
+  {
     title: "yes",
     community: "Softeng701",
-    upi: "shfdj364",
+    upi: "abcd123",
     time: "12:12",
     text: "I love react and react libraries but I have never used Semantic UI...  I heard from a teammate  that it’s pretty easy to use and has a lot of documentation so hopefully I’ll be fine. Any tips...",
     upvotes: 24,
     downvotes: 23,
-    images: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXsGYUdnO7UWSuZV_wMfyq-ChTkvfHjMMUcA&usqp=CAU", "https://nz3.architecturemedia.net/site_media/media/cache/52/47/5247e80bab99158eecfb84da220fe7b1.jpg"]
-},
-{
+    images: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXsGYUdnO7UWSuZV_wMfyq-ChTkvfHjMMUcA&usqp=CAU",
+      "https://nz3.architecturemedia.net/site_media/media/cache/52/47/5247e80bab99158eecfb84da220fe7b1.jpg",
+    ],
+  },
+  {
     title: "no",
-    community: "Softeng701",
+    community: "Softeng751",
     upi: "shfdj364",
     time: "12:12",
-    text: "I love react and react libraries but I have never used Semantic UI...  I heard from a teammate  that it’s pretty easy to use and has a lot of documentation so hopefully I’ll be fine. Any tips...",
+    text: "High performance computing seems pretty interesting, but also difficult to learn. Also how do you even pronounce parallelisation",
     upvotes: 24,
     downvotes: 23,
-    images: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXsGYUdnO7UWSuZV_wMfyq-ChTkvfHjMMUcA&usqp=CAU", "https://nz3.architecturemedia.net/site_media/media/cache/52/47/5247e80bab99158eecfb84da220fe7b1.jpg"]
-},
-{
+    images: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXsGYUdnO7UWSuZV_wMfyq-ChTkvfHjMMUcA&usqp=CAU",
+      "https://nz3.architecturemedia.net/site_media/media/cache/52/47/5247e80bab99158eecfb84da220fe7b1.jpg",
+    ],
+  },
+  {
     title: "hi",
-    community: "Softeng701",
-    upi: "shfdj364",
+    community: "Compsys701",
+    upi: "das8089",
     time: "12:12",
     text: "I love react and react libraries but I have never used Semantic UI...  I heard from a teammate  that it’s pretty easy to use and has a lot of documentation so hopefully I’ll be fine. Any tips...",
     upvotes: 24,
     downvotes: 23,
-    images: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXsGYUdnO7UWSuZV_wMfyq-ChTkvfHjMMUcA&usqp=CAU", "https://nz3.architecturemedia.net/site_media/media/cache/52/47/5247e80bab99158eecfb84da220fe7b1.jpg"]
-},
-{
+    images: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXsGYUdnO7UWSuZV_wMfyq-ChTkvfHjMMUcA&usqp=CAU",
+      "https://nz3.architecturemedia.net/site_media/media/cache/52/47/5247e80bab99158eecfb84da220fe7b1.jpg",
+    ],
+  },
+  {
     title: "title4",
     community: "Softeng701",
-    upi: "shfdj364",
+    upi: "sass2364",
     time: "12:12",
     text: "I love react and react libraries but I have never used Semantic UI...  I heard from a teammate  that it’s pretty easy to use and has a lot of documentation so hopefully I’ll be fine. Any tips...",
     upvotes: 24,
     downvotes: 23,
-    images: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXsGYUdnO7UWSuZV_wMfyq-ChTkvfHjMMUcA&usqp=CAU", "https://nz3.architecturemedia.net/site_media/media/cache/52/47/5247e80bab99158eecfb84da220fe7b1.jpg"]
-}
-]
+    images: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXsGYUdnO7UWSuZV_wMfyq-ChTkvfHjMMUcA&usqp=CAU",
+      "https://nz3.architecturemedia.net/site_media/media/cache/52/47/5247e80bab99158eecfb84da220fe7b1.jpg",
+    ],
+  },
+];
 const Posts = () => {
+  const navigate = useNavigate();
+  //creating a state for filtered results
+  const [filteredResults, setFilteredResults] = useState(posts);
 
-    //creating a state for filtered results
-    const [filteredResults, setFilteredResults] = useState(posts);
+  //creating a state for search query in searchbar
+  const [searchQuery, setSearchQuery] = useState("");
 
-    //Function for filtering posts by title when user inputs text in the search bar, else all posts will be shown
-    const searchItem = (searchValue) => {
-        if (searchValue !== '') {
-            const filteredSearch = posts.filter((item) => { return item.title.toLowerCase().includes(searchValue.toLowerCase()) })
-            setFilteredResults(filteredSearch)
-        }
-        else {
-            setFilteredResults(posts)
-        }
-    } 
-  
-    return (
-        <Container maxWidth="md">
-            <Box className="p-toprow">
-                <h1 className="p-title">Posts</h1>
-                <Box className="p-postbutton">
-                    <Button variant="contained">New Post</Button>
+  //Function for filtering posts by title, community, text, or upi when user inputs text in the search bar, else all posts will be shown
+  const searchItem = (searchValue) => {
+    setSearchQuery(searchValue);
+    if (searchValue !== "") {
+      const filteredSearch = posts.filter((item) => {
+        return (
+          item.title.toLowerCase().includes(searchValue.toLowerCase()) ||
+          item.community.toLowerCase().includes(searchValue.toLowerCase()) ||
+          item.text.toLowerCase().includes(searchValue.toLowerCase()) ||
+          item.upi.toLowerCase().includes(searchValue.toLowerCase())
+        );
+      });
+      setFilteredResults(filteredSearch);
+    } else {
+      setFilteredResults(posts);
+    }
+  };
 
-                </Box>
-            </Box>
-            <hr style={{ border: "3px solid white" }} />
-            <Box className="p-search">
-                <FormControl className="p-searchbar" variant="filled">
-                    <InputLabel htmlFor="search">Search Posts</InputLabel>
-                    <OutlinedInput
-                        id="search"
-                        endAdornment={
-                            <InputAdornment position="end">
-                                <Search />
-                            </InputAdornment>
-
-                        }
-                        //when user inputs something into the search bar this calls the searchItem function to filter out the posts
-                        //based on the input
-                        onChange={(e) => searchItem(e.target.value)}
-                    />
-                </FormControl>
-            </Box>
-
-            {filteredResults && filteredResults.map(post =>
-                <Box className="p-post">
-                    <PostPreviewComponent post={post} />
-                </Box>)}
-        </Container>
-    )
+  return (
+    <Container maxWidth="md">
+      <Box className="p-toprow">
+        <h1 className="p-title">Posts</h1>
+        <Box className="p-postbutton">
+          <Button variant="contained" onClick={() => navigate("/newpost")}>
+            New Post
+          </Button>
+        </Box>
+      </Box>
+      <hr style={{ border: "3px solid white" }} />
+      <Box className="p-search">
+        <FormControl className="p-searchbar" variant="filled">
+          <InputLabel htmlFor="search">Search Posts</InputLabel>
+          <OutlinedInput
+            id="search"
+            endAdornment={
+              <InputAdornment position="end">
+                <Search />
+              </InputAdornment>
+            }
+            //when user inputs something into the search bar this calls the searchItem function to filter out the posts
+            //based on the input
+            onChange={(e) => searchItem(e.target.value)}
+          />
+        </FormControl>
+      </Box>
+      {searchQuery.length > 0 && (
+        <p>
+          <b>{filteredResults.length} results found based on search query "{searchQuery}"</b>
+        </p>
+      )}
+      {filteredResults &&
+        filteredResults.map((post) => (
+          <Box className="p-post">
+            <PostPreviewComponent post={post} />
+          </Box>
+        ))}
+    </Container>
+  );
 };
 
 export default Posts;

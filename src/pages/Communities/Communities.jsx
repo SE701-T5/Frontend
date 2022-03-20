ee106ff47e8478783758cecfdf178befb3963b11
yuchendong <68779035+yuchendong13@users.noreply.github.com>
2022-03-20 14:50:50 +1300

    Added communities search
    - Added search functionality similar to Posts page
    - Changed static results text to match search query

--- src/pages/Communities/Communities.jsx
+++ src/pages/Communities/Communities.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./communities.css";
 import Container from "@mui/material/Container";
 import {Box, FormControl, InputLabel, OutlinedInput, InputAdornment, Button} from "@mui/material";
 import CommunityPreviewComponent from "../../components/communityPreviewComponent/CommunityPreviewComponent.jsx";
 import { Search } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 
 const buttonStyle = {
   backgroundColor: '#059EF4',
@@ -42,11 +43,34 @@ const Communities = () => {
     }
   ];
 
+  const navigate = useNavigate();
+  //creating a state for filtered results
+  const [filteredResults, setFilteredResults] = useState(communityPreviews);
+
+  //creating a state for search query in searchbar
+  const [searchQuery, setSearchQuery] = useState("");
+
+  //Function for filtering posts by title, community, text, or upi when user inputs text in the search bar, else all posts will be shown
+  const searchItem = (searchValue) => {
+    setSearchQuery(searchValue);
+    if (searchValue !== "") {
+      const filteredSearch = communityPreviews.filter((item) => {
+        return (
+          item.community.toLowerCase().includes(searchValue.toLowerCase()) ||
+          item.description.toLowerCase().includes(searchValue.toLowerCase())
+        );
+      });
+      setFilteredResults(filteredSearch);
+    } else {
+      setFilteredResults(communityPreviews);
+    }
+  };
+
   return (
     <Container maxWidth="md">
       <div className="com-titleContainer">
         <h1 className="com-Title">Communities</h1>
-        <Button variant="contained" style={buttonStyle}>New Community</Button>
+        <Button variant="contained" style={buttonStyle} onClick={() => navigate("/NewCommunity")}>New Community</Button>
         <div className="com-Divide">
           <hr className="com-Divider"></hr>
         </div>
@@ -62,15 +86,17 @@ const Communities = () => {
                     <InputAdornment position="end">
                         <Search />
                     </InputAdornment>
-
                 }
+                onChange={(e) => searchItem(e.target.value)}
             />
           </FormControl>
         </Box>
-        <div className="com-result">
-          <b>Based on search query "SOFTENG"</b>
-        </div>
-        {communityPreviews.map((communityEntry) => (
+        {searchQuery.length > 0 && (
+        <p>
+          <b>{filteredResults.length} results found based on search query "{searchQuery}"</b>
+        </p>
+        )}
+        {filteredResults && filteredResults.map((communityEntry) => (
           <CommunityPreviewComponent communityEntry={communityEntry}/>
         ))} 
       </div>

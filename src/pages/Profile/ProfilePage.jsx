6e68a5c9a27950271f94e49359469ba7549f671b
FriedhelmWS <352063465@qq.com>
2022-03-26 00:32:11 +1300

    Change email/password form

--- src/pages/Profile/ProfilePage.jsx
+++ src/pages/Profile/ProfilePage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./profilePage.css";
-import Container from "@mui/material/Container";
+import { Container, Modal } from "@mui/material";
 import { Search } from "@mui/icons-material";
 import {
   Box,
@@ -80,8 +80,14 @@ const ProfilePage = () => {
   //creating a state for search query in searchbar
   const [searchQuery, setSearchQuery] = useState("");
 
+  //handle change password/email modal opening
+  const [open, setOpen] = useState(false);
+
   return (
     <Container maxWidth="sm">
+      {/* Just a ad-hoc button, feel free to change */}
+      <button onClick={() => setOpen(true)}>Change Email/Password</button>
+      <ChangeModal open={open} handleClose={() => setOpen(false)} />
       <Box className="p-toprow" style={{ marginBottom: "20px" }}>
         <h1 className="p-title">Post Search</h1>
       </Box>
@@ -128,5 +134,70 @@ const ProfilePage = () => {
     </Container>
   );
 };
+
+function ChangeModal({ open, handleClose }) {
+  return (
+    <Modal open={open} onClose={handleClose}>
+      <div className="absolute w-3/4 h-full max-w-3xl p-4 transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 md:h-auto md:w-full">
+        <div className="relative bg-gray-100 shadow rounded-3xl">
+          <div className="grid pt-5 sm:border-r-1 md:grid-cols-2">
+            <div className="py-5 border-gray-300 border-dashed md:border-r-2">
+              <div className="flex flex-col gap-2 px-1 mx-8">
+                <div className="flex flex-row justify-center pb-2 text-lg font-bold text-primary-blue">
+                  Change Login Email
+                </div>
+                <input
+                  className="block w-full h-8 px-4 py-3 mb-6 leading-tight text-gray-700 bg-white border shadow-sm rounded-xl"
+                  placeholder="Old Email"
+                />
+                <input
+                  className="block w-full h-8 px-4 py-3 mb-2 leading-tight text-gray-700 bg-white border shadow-sm rounded-xl"
+                  placeholder="New Email"
+                />
+                <input
+                  className="block w-full h-8 px-4 py-3 mb-2 leading-tight text-gray-700 bg-white border shadow-sm rounded-xl"
+                  placeholder="Confirm New Email"
+                />
+                <button className="h-8 mt-2 font-bold text-white shadow-lg rounded-xl bg-primary-blue">
+                  Submit!
+                </button>
+              </div>
+            </div>
+            <div className="py-5">
+              <div className="flex flex-col gap-2 px-1 mx-8">
+                <div className="flex flex-row justify-center pb-2 text-lg font-bold text-primary-blue">
+                  Change Login Email
+                </div>
+                <input
+                  className="block w-full h-8 px-4 py-3 mb-6 leading-tight text-gray-700 bg-white border shadow-sm rounded-xl"
+                  type="password"
+                  placeholder="Old Password"
+                />
+                <input
+                  className="block w-full h-8 px-4 py-3 mb-2 leading-tight text-gray-700 bg-white border shadow-sm rounded-xl"
+                  type="password"
+                  placeholder="New Password"
+                />
+                <input
+                  className="block w-full h-8 px-4 py-3 mb-2 leading-tight text-gray-700 bg-white border shadow-sm rounded-xl"
+                  type="password"
+                  placeholder="Confirm New Password"
+                />
+                <button className="h-8 mt-2 font-bold text-white shadow-lg rounded-xl bg-primary-blue">
+                  Submit!
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-row justify-center">
+            <button className="w-full h-8 mx-8 mt-2 mb-10 font-bold text-gray-500 bg-white shadow-lg rounded-xl md:w-2/5">
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+}
 
 export default ProfilePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: { "primary-blue": "rgba(25, 70, 123, 0.97)" },
+    },
   },
   plugins: [],
 };

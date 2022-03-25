ba03b0c7a69109820c3d8d6374a603b55a8cddb3
Fraser McCallum <55963097+frasermcc9@users.noreply.github.com>
2022-03-25 16:07:37 +1300

    Setup API Hook, add prettier, add basic JS type-checking with TS (#90)
    * Add API hooks
    
    * Reset code to prettier configuration
    
    * Call requests with correct URL
    
    * Remove unused method
    
    * Add ability to modify URL at mutation callsite
    
    * Remove now defunct api function

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -3,53 +3,55 @@ import "./comment.css";
 import { ArrowUpward, ArrowDownward } from "@mui/icons-material";
 import { Box, IconButton, Avatar } from "@mui/material";
 
-
 const Comment = ({ comment }) => {
+  return (
+    <>
+      <Box className="post-TopRow">
+        <Box className="post-avatar">
+          <Avatar
+            alt="name"
+            src="https://i.pinimg.com/originals/7c/c7/a6/7cc7a630624d20f7797cb4c8e93c09c1.png"
+          />
+        </Box>
+        <Box className="post-details">
+          <p className="post-commentupi">{comment.commentupi}</p>
+          <p className="post-commenttime">{comment.commenttime}</p>
+        </Box>
+      </Box>
 
-    return (
-        <>
-            <Box className="post-TopRow">
-                <Box className="post-avatar">
-                    <Avatar alt="name" src="https://i.pinimg.com/originals/7c/c7/a6/7cc7a630624d20f7797cb4c8e93c09c1.png" />
-                </Box>
-                <Box className="post-details">
-                    <p className="post-commentupi">{comment.commentupi}</p>
-                    <p className="post-commenttime">{comment.commenttime}</p>
-                </Box>
-            </Box>
+      <Box className="post-MiddleRow">
+        <Box className="post-comment">
+          <p className="post-text">{comment.comment}</p>
+        </Box>
+      </Box>
 
-            <Box className="post-MiddleRow">
-                <Box className="post-comment">
-                    <p className="post-text">{comment.comment}</p>
-                </Box>
+      <Box className="post-LastRow">
+        <Box className="post-votes">
+          <IconButton>
+            <ArrowUpward
+              sx={{
+                color: "rgba(0,128,167,0.35)",
+                "&:hover": { color: "#0080A7" },
+              }}
+            />
+          </IconButton>
 
-            </Box>
+          <p className="post-numofvotes">
+            {comment.commentupvotes - comment.commentdownvotes}
+          </p>
 
-            <Box className="post-LastRow">
-                <Box className="post-votes">
-                    <IconButton>
-                        <ArrowUpward
-                            sx={{
-                                color: "rgba(0,128,167,0.35)",
-                                "&:hover": { color: "#0080A7" },
-                            }}
-                        />
-                    </IconButton>
-
-                    <p className="post-numofvotes">{comment.commentupvotes - comment.commentdownvotes}</p>
-
-                    <IconButton>
-                        <ArrowDownward
-                            sx={{
-                                color: "rgba(0,128,167,0.35)",
-                                "&:hover": { color: "#0080A7" },
-                            }}
-                        />
-                    </IconButton>
-                </Box>
-            </Box>
-        </>
-    );
+          <IconButton>
+            <ArrowDownward
+              sx={{
+                color: "rgba(0,128,167,0.35)",
+                "&:hover": { color: "#0080A7" },
+              }}
+            />
+          </IconButton>
+        </Box>
+      </Box>
+    </>
+  );
 };
 
-export default Comment
+export default Comment;

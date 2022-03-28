925bd9252f81029240bd75a753c732ee9a48cc8c
Samuel Chen <68937749+Sparye@users.noreply.github.com>
2022-03-29 11:17:51 +1300

    connect endpoints
    connect user login and signup endpoints

--- src/pages/LoginRegister/LoginPage.jsx
+++ src/pages/LoginRegister/LoginPage.jsx
@@ -4,27 +4,47 @@ import { useNavigate } from "react-router-dom";
 import AuthContext from "../../context/AuthProvider";
 import LoginForm from "./LoginForm";
 import Homepage from "../Homepage";
+import { useMutation } from "../../hooks/useApi";
 export default function LoginPage() {
   const navigate = useNavigate();
-
+  const userLogin = useMutation("/users/login", {
+    method: "post",
+  });
   const { userDetails, login, authorized } = useContext(AuthContext);
 
-  const LoginFormDetail = (details) => {
-    const founduser = userDetails.find((user) => user.email === details.email);
-    if (founduser) {
-      if (details.password === founduser.password) {
-        console.log("loged in");
-        alert("Logged in!");
-        login();
-        navigate("/homepage");
-      } else {
-        console.log("Your password is wrong!");
-        alert("Your password is wrong!");
-      }
-    } else {
-      //console.log("No such account, Please register first!");
-      alert("No such account, Please register first!");
+  const LoginFormDetail = async (details) => {
+    const loginDetails = {
+      data: {
+        email: details.email,
+        plaintextPassword: details.password,
+      },
+    };
+    try {
+      const response = await userLogin(loginDetails);
+      console.log(response);
+      localStorage.setItem("id", response.userID);
+      localStorage.setItem("email", details.email);
+      localStorage.setItem("authToken", response.authToken);
+      login();
+      navigate("/homepage");
+    } catch (error) {
+      console.log("failed to login");
     }
+    // const foundUser = userDetails.find((user) => user.email === details.email);
+    // if (foundUser) {
+    //   if (details.password === foundUser.password) {
+    //     console.log("loged in");
+    //     alert("Logged in!");
+    //     login();
+    //     navigate("/homepage");
+    //   } else {
+    //     console.log("Your password is wrong!");
+    //     alert("Your password is wrong!");
+    //   }
+    // } else {
+    //   //console.log("No such account, Please register first!");
+    //   alert("No such account, Please register first!");
+    // }
   };
   return (
     <>
--- src/pages/LoginRegister/RegisterPage.jsx
+++ src/pages/LoginRegister/RegisterPage.jsx
@@ -3,30 +3,51 @@ import "./LoginPage.css";
 import AuthContext from "../../context/AuthProvider";
 import RegisterForm from "./RegisterForm";
 import { useNavigate } from "react-router-dom";
+import { useMutation } from "../../hooks/useApi";
+import axios from "axios";
 
 export default function RegisterPage() {
   const navigate = useNavigate();
 
   const { userDetails, setUserDetails } = useContext(AuthContext);
+  const userSignUp = useMutation("/users", {
+    method: "post",
+  });
 
-  const Register = (details) => {
-    console.log(details);
-    console.log(userDetails);
+  const Register = async (details) => {
+    //console.log(details);
+    //console.log(userDetails);
     if (!userDetails.find((user) => user.email === details.email)) {
       if (details.password1 === details.password2) {
-        console.log("Registered successfully!");
-        console.log(userDetails);
-        setUserDetails((preuserDetails) => [
-          ...preuserDetails,
-          {
-            email: details.email,
+        const signUpDetails = {
+          data: {
             username: details.username,
-            password: details.password1,
+            displayName: details.username,
+            email: details.email,
+            plaintextPassword: details.password1,
           },
-        ]);
-        console.log(userDetails);
-        alert("Registered successfully! Please Log in!");
-        navigate("/login");
+        };
+        try {
+          const response = await userSignUp(signUpDetails);
+          const data = await response.userData;
+          if (data.status === 201) {
+            const res = data.res;
+            console.log("Registered successfully!");
+            console.log(userDetails);
+            setUserDetails((preuserDetails) => [
+              ...preuserDetails,
+              {
+                email: details.email,
+                username: details.username,
+                password: details.password1,
+              },
+            ]);
+            console.log(userDetails);
+            alert("Registered successfully! Please Log in!");
+          }
+        } catch (error) {
+          console.log("failed to sign up");
+        }
       } else {
         console.log("Your password doesn't match!");
         alert("Your password doesn't match!");
